df36d0b88a88fdef981445229dd0b3fb3b8ab6ed
dphelan <phelan.daniel@orbcomm.com>
2024-05-20 22:49:44 +0100

    Base Spring Boot application

---
+++ src/main/java/com/daniel/productcatalog/ProductCatalogManagerApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/productcatalog/ProductCatalogManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
